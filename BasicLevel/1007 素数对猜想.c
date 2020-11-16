fa5f3bca5bd662bb5be46190b68c6ea7c7e27634
czp <czp3009@gmail.com>
2020-11-16 17:08:04 +0800

    1007

---
+++ BasicLevel/1007 素数对猜想.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <memory.h>
+#include <math.h>
+
+#define NOT_CHECKED 0
+#define IS_PRIMARY_NUMBER 1
+#define NOT_PRIMARY_NUMBER -1
+
+int isPrimaryNumber(int number, const int *numberAxis) {
+    int upperLimit = sqrt(number); // NOLINT(cppcoreguidelines-narrowing-conversions)
+    for (int i = 2; i <= upperLimit; i++) {
+        if (numberAxis[i] != IS_PRIMARY_NUMBER) continue;
+        if (number % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n); // NOLINT(cert-err34-c)
+
+    //Euler
+    int numberAxisLength = n + 1;
+    int numberAxis[numberAxisLength];
+    memset(numberAxis, NOT_CHECKED, sizeof(int) * numberAxisLength);
+    for (int i = 2; i < numberAxisLength; i++) {
+        if (numberAxis[i] != NOT_CHECKED) continue;
+        if (isPrimaryNumber(i, numberAxis)) {
+            numberAxis[i] = IS_PRIMARY_NUMBER;
+            for (int j = 2; i * j < numberAxisLength; j++) {
+                numberAxis[i * j] = NOT_PRIMARY_NUMBER;
+            }
+        } else {
+            numberAxis[i] = NOT_PRIMARY_NUMBER;
+        }
+    }
+
+    //output
+    int count = 0;
+    for (int i = 4; i < numberAxisLength; i++) {
+        if (numberAxis[i] == IS_PRIMARY_NUMBER && numberAxis[i - 2] == IS_PRIMARY_NUMBER) {
+            count++;
+            i++;    //skip next number, it can't be primary number
+        }
+    }
+    printf("%d", count);
+
+    return 0;
+}
